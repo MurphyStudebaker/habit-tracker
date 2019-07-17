ea17e52e663d0acf8e4d6b212b8083ebb058f860
MurphyStudebaker <murphy.stude@gmail.com>
2019-07-17 12:43:31 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
